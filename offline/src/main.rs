6280fda8e5431f221ae16f13dd5c6c954886527b
Adam Berg <adam@xyzdigital.com>
2021-11-14 10:00:34 -0800

    Add /del command

--- offline/src/main.rs
+++ offline/src/main.rs
@@ -18,18 +18,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
       io::stdin().read_line(&mut buffer)?;
 
       let trimmed_body = buffer.trim();
-      if trimmed_body == "list" {
-        let mut stmt = conn.prepare("SELECT body from notes")?;
+      let split_whitespace = trimmed_body.split_whitespace();
+      if trimmed_body == "/list" {
+        let mut stmt = conn.prepare("SELECT id, body from notes")?;
         let mut rows = stmt.query(rusqlite::params![])?;
         while let Some(row) = rows.next()? {
-          let body: String = row.get(0)?;
-          println!("{}", body.to_string());
+          let id: i32 = row.get(0)?;
+          let body: String = row.get(1)?;
+          println!("{} {}", id, body.to_string());
         }
+      } else if trimmed_body.contains("/del") {
+        let id = split_whitespace.last();
+        conn.execute("delete from notes where id = (?1)", [id])?;
       }
       else if trimmed_body == "" {
         running = false;
       } else {
-        conn.execute("INSERT INTO notes (body) values (?1)", [trimmed_body]);
+        conn.execute("INSERT INTO notes (body) values (?1)", [trimmed_body])?;
       }
     }
 
