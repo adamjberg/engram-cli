15039baba431febca6d3239f4775715d4565cd25
Adam Berg <adam@xyzdigital.com>
2021-11-14 08:09:46 -0700

    Add list command

--- offline/src/main.rs
+++ offline/src/main.rs
@@ -16,10 +16,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     while running == true {
     let mut buffer = String::new();
       io::stdin().read_line(&mut buffer)?;
-      if buffer.trim() == "" {
+
+      let trimmed_body = buffer.trim();
+      if trimmed_body == "list" {
+        let mut stmt = conn.prepare("SELECT body from notes")?;
+        let mut rows = stmt.query(rusqlite::params![])?;
+        while let Some(row) = rows.next()? {
+          let body: String = row.get(0)?;
+          println!("{}", body.to_string());
+        }
+      }
+      else if trimmed_body == "" {
         running = false;
       } else {
-        conn.execute("INSERT INTO notes (body) values (?1)", [buffer]);
+        conn.execute("INSERT INTO notes (body) values (?1)", [trimmed_body]);
       }
     }
 
