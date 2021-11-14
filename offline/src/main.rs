c3eadbd27e7026e616f26b824af3be1270e1b045
Adam Berg <adam@xyzdigital.com>
2021-11-14 10:30:26 -0800

    Add /edit command

--- offline/src/main.rs
+++ offline/src/main.rs
@@ -18,8 +18,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
       io::stdin().read_line(&mut buffer)?;
 
       let trimmed_body = buffer.trim();
-      let split_whitespace = trimmed_body.split_whitespace();
-      if trimmed_body == "/list" {
+      let cmd_split = trimmed_body.split_once(" ");
+
+      let mut cmd = trimmed_body;
+      let mut msg = "";
+      if cmd_split != None {
+        cmd = cmd_split.unwrap().0;
+        msg = cmd_split.unwrap().1;
+      }
+      
+      if cmd == "/list" {
         let mut stmt = conn.prepare("SELECT id, body from notes")?;
         let mut rows = stmt.query(rusqlite::params![])?;
         while let Some(row) = rows.next()? {
@@ -27,11 +35,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
           let body: String = row.get(1)?;
           println!("{} {}", id, body.to_string());
         }
-      } else if trimmed_body.contains("/del") {
-        let id = split_whitespace.last();
+      } else if cmd == "/del" {
+        let id = msg;
         conn.execute("delete from notes where id = (?1)", [id])?;
-      }
-      else if trimmed_body == "" {
+      } else if cmd == "/edit" {
+        let msg_split = msg.split_once(" ").unwrap();
+        let id = msg_split.0;
+        let body = msg_split.1;
+
+        conn.execute("update notes set body = (?1) where id = (?2)", [body, id])?;
+      } else if cmd == "" {
         running = false;
       } else {
         conn.execute("INSERT INTO notes (body) values (?1)", [trimmed_body])?;
